5d691559c90654f6bace50c5451a9c2796e18620
Arsalan Balzhinimaev <arsalashka@gmail.com>
2022-02-10 19:39:39 +0300

    Initial Commit

---
+++ Lesson_2_4_PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_2_4_PersonalityQuiz
+//
+//  Created by Arsalan Balzhinimaev on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
